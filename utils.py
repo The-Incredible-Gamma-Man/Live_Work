a9647d6f99c5613063a9c4d29382a0b639d78eb6
The-Incredible-Gamma-Man <80361135+The-Incredible-Gamma-Man@users.noreply.github.com>
2024-03-14 14:07:14 +0000

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 from PIL import Image
-import face_recognition as faceRegLib
+import face_recognition
 import io
 
 def read_image(file_stream):
@@ -20,13 +20,13 @@ def preprocess_image(image):
 
 def get_face_encodings(image):
     # Given an image, return the 128-dimension face encoding for each face in the image.
-    face_locations = faceRegLib.face_locations(image)
-    face_encodings = faceRegLib.face_encodings(image, face_locations)
+    face_locations = face_recognition.face_locations(image)
+    face_encodings = face_recognition.face_encodings(image, face_locations)
     return face_encodings
 
 def compare_faces(encodings1, encodings2):
     # Compare two lists of face encodings and return True if there are matches
     # This assumes that each list of encodings is from the same person
     # Adjust the tolerance parameter as needed
-    matches = faceRegLib.compare_faces(encodings1, encodings2, tolerance=0.6)
+    matches = face_recognition.compare_faces(encodings1, encodings2, tolerance=0.6)
     return any(matches)
