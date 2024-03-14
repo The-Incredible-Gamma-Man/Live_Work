34a1db4dd5fddd6f35de85dad039e846738f8ce8
The-Incredible-Gamma-Man <80361135+The-Incredible-Gamma-Man@users.noreply.github.com>
2024-03-14 14:08:57 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-from face_recognition.compare import compare_faces
+from face_recognition.compare import compare_faces_from_files
 
 app = Flask(__name__)
 
@@ -10,7 +10,7 @@ def index():
         # Process uploaded images
         image1 = request.files['image1']
         image2 = request.files['image2']
-        match = compare_faces(image1, image2)
+        match = compare_faces_from_files(image1, image2)
     return render_template('index.html', match=match)
 
 if __name__ == '__main__':
